b3f52a6190aee1e5e5d3d6ac5e84b0cdfc16e4c1
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-27 17:17:01 +0300

    Refactor JS into different modules, Style changes

---
+++ modules/init-page.js
@@ -0,0 +1,31 @@
+const initializePage = () => {
+
+  // ---- navigation eventlisteners ---- //
+  const formContainer = document.querySelector('.book-form');
+  const bookListSection = document.querySelector('.book-list-section');
+  const contactSection = document.querySelector('.contact-section');
+
+  const listLink = document.querySelector('#menu-list-btn');
+  const AddNewLink = document.querySelector('#menu-add-btn');
+  const contactLink = document.querySelector('#menu-contact-btn');
+
+  listLink.addEventListener('click', () => {
+    bookListSection.style.display = 'block';
+    formContainer.style.display = 'none';
+    contactSection.style.display = 'none';
+  });
+
+  AddNewLink.addEventListener('click', () => {
+    bookListSection.style.display = 'none';
+    formContainer.style.display = 'flex';
+    contactSection.style.display = 'none';
+  });
+
+  contactLink.addEventListener('click', () => {
+    bookListSection.style.display = 'none';
+    contactSection.style.display = 'flex';
+    formContainer.style.display = 'none';
+  });
+}
+
+export default initializePage;
