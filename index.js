cd0d79eb4e8a23ce773bf83f78b6ba126b47c22b
Sasan Moshirabadi <sasan.dev89@gmail.com>
2023-03-28 10:31:44 +0300

    Fix init-page/Remove bookload from index.js

--- index.js
+++ index.js
@@ -4,7 +4,3 @@ import loadDate from './modules/load-date.js';
 
 initializePage();
 loadDate();
-
-// ---- create and load books on page ---- //
-const bookObj = new BookList();
-bookObj.loadBooks();
